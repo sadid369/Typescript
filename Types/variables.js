9acc9a6356db010210947660f83bba310d770158
sadid369 <sadid.jones@gmail.com>
2024-02-06 01:20:20 +0600

    Object Chapter to Start

---
+++ Types/variables.js
@@ -0,0 +1,11 @@
+function square(num) {
+    if (num === void 0) { num = "sadid"; }
+    return "hello ".concat(num);
+}
+console.log(square());
+var addnumber = function () { return 5; };
+var colors = ['red', 'orange', 'yellow'];
+var upColor = colors.map(function (color) {
+    return color.toUpperCase();
+});
+console.log(upColor);
---
+++ Types/variables.ts
@@ -0,0 +1,14 @@
+function square(num:string="sadid"):string {
+    return `hello ${num}`;
+}
+
+console.log(square());
+const addnumber = ():number=>{return 5}
+
+const colors = ['red', 'orange', 'yellow']
+
+const upColor= colors.map(color=>{
+ return   color.toUpperCase()
+})
+
+console.log(upColor);
